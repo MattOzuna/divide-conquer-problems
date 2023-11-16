f9ea2c61c69348a16abc50091e329e3ec84a8444
Matt Ozuna <m.ozuna22@gmail.com>
2023-11-16 16:16:19 -0500

    completed find-rotation-count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,27 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+    if(arr[0] < arr[arr.length - 1])
+        return 0
+    
+    pivotPoint = findPivotPoint(arr)
+    return pivotPoint
+}
+
+function findPivotPoint(arr){
+    if(arr[0] < arr[arr.length - 1])
+        return 0
+
+    let leftIdx = 0
+    let rightIdx = arr.length -1
+
+    while (leftIdx <= rightIdx){
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2)
+        if (arr[middleIdx] > arr[middleIdx +1])
+            return middleIdx + 1
+        else if (arr[leftIdx] <= arr[middleIdx])
+            leftIdx = middleIdx + 1
+        else
+            rightIdx = middleIdx - 1
+    }
 }
 
 module.exports = findRotationCount
