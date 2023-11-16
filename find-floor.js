0f26f727af8de75c98e5facc738fda1371b799f0
Matt Ozuna <m.ozuna22@gmail.com>
2023-11-16 12:15:48 -0500

    completed find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,25 @@
-function findFloor() {
+function findFloor(arr, val) {
+    let leftIdx = 0;
+    let rightIdx = arr.length - 1;
+    closestVal = 0
+
+    while (leftIdx <= rightIdx) {
+        let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+        let middleVal = arr[middleIdx];
+
+        if (middleVal < val) {
+            leftIdx = middleIdx + 1;
+            closestVal = middleVal
+        } else if (middleVal > val) {
+            rightIdx = middleIdx - 1;
+        } else {
+            return middleIdx;
+        }
+    }
   
+    if(closestVal < val)
+        return closestVal
+    return -1;
 }
 
 module.exports = findFloor
